4eeb47c169f3b460ebe1895f1a1040636ade8a70
kenrazalan <kennethrazalan29@gmail.com>
2021-01-15 13:33:12 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+const App = () => 
+
+<div className="App">
+    <div className="group">
+        {/* <div className="steps">
+            <p className="sacola">Sacola</p>
+            <p className="pagamento">Pagamento</p>
+            <p className="confirmacao">Confirmation</p>
+        </div>
+        <div className="produtos"></div>
+        <div className="total"></div>
+        <div className="extra-large"></div> */}
+        <div className="fundo"></div>
+
+    </div>
+</div>
+
+export default App
