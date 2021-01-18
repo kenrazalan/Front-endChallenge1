4ad1b1b9267dc0ca9c8fb49059e9e569b650c1e9
kenneth <kennethrazalan29@gmail.com>
2021-01-19 00:36:02 +0800

    axios api

--- src/App.js
+++ src/App.js
@@ -1,77 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import item from './assets/item1.png'
+import axios from 'axios'
+import { clearConfigCache } from 'prettier'
 
-const App = () => 
+const App = () => {
+  const [data, setData] = useState([])
 
-<div className="App">
-  
-       
-        <div className="fundo">
+  axios
+    .get('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
+    .then((response) => {
+      setData(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 
+  return (
+    <div className="App">
+      <div className="fundo">
         <div className="steps">
-            <p className="cart">Cart</p>
-            <p className="payment">Payment</p>
-            <p className="confirmation">Confirmation</p>
+          <p className="cart">Cart</p>
+          <p className="payment">Payment</p>
+          <p className="confirmation">Confirmation</p>
         </div>
         <div className="produtos">
-            <div className="products-label">Products</div>
-            <div className="products">
-                <div className="products-details"> 
-                    <img src={item} className="item"></img>
-                    <div className="price-description">
-                        <div className="item-description">
-                        L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
-                        </div>
-                        <div className="price">R$ 225,90 </div>
-                    </div>
+          <div className="products-label">Products</div>
+          <div className="products">
+            <div className="products-details">
+              <img src={item} className="item"></img>
+              <div className="price-description">
+                <div className="item-description">
+                  L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
                 </div>
+                <div className="price">R$ 225,90 </div>
+              </div>
             </div>
-            <div className="products">
-                <div className="products-details">
-                    <img src={item} className="item"></img>
-                    <div className="price-description">
-                        <div className="item-description">
-                        L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
-                        </div>
-                        <div className="price">R$ 225,90 </div>
-                    </div>
+          </div>
+          <div className="products">
+            <div className="products-details">
+              <img src={item} className="item"></img>
+              <div className="price-description">
+                <div className="item-description">
+                  L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
                 </div>
+                <div className="price">R$ 225,90 </div>
+              </div>
             </div>
-            <div className="products">
-                <div className="products-details">
-                    <img src={item} className="item"></img>
-                    <div className="price-description">
-                        <div className="item-description">
-                        L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
-                        </div>
-                        <div className="price">R$ 225,90 </div>
-                    </div>
+          </div>
+          <div className="products">
+            <div className="products-details">
+              <img src={item} className="item"></img>
+              <div className="price-description">
+                <div className="item-description">
+                  L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium
                 </div>
+                <div className="price">R$ 225,90 </div>
+              </div>
             </div>
+          </div>
         </div>
         <div className="total">
-            <div className="total-details">
-                <div className="subtotal">Subtotal</div>
-                <div className="shipping-total">Shipping Total</div>
-                <div className="discount">Discount</div>
-                <div className="total-total">Total</div>
-            </div>
-            <div className="total-value">
-                <div className="subtotal-value">Subtotal</div>
-                <div className="shipping-total-value">Shipping Total</div>
-                <div className="discount-value">Discount</div>
-                <div className="total-total-value">Total</div>
-            </div>
+          <div className="total-details">
+            <div className="subtotal">Subtotal</div>
+            <div className="shipping-total">Shipping Total</div>
+            <div className="discount">Discount</div>
+            <div className="total-total">Total</div>
+          </div>
+          <div className="total-value">
+            <div className="subtotal-value">Subtotal</div>
+            <div className="shipping-total-value">Shipping Total</div>
+            <div className="discount-value">Discount</div>
+            <div className="total-total-value">Total</div>
+          </div>
         </div>
         <div className="extra-large">
-            <p className="payment-button">Go to Payment</p>
-        </div> 
-
-
+          <p className="payment-button">Go to Payment</p>
         </div>
-
-    
-</div>
-
+      </div>
+    </div>
+  )
+}
 export default App
